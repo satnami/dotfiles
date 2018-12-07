7e274162cba4daac02a4d6351ddcb5c9cecf0d67
Mhd Sami Almouhtaseb <mssatnami@gmail.com>
2018-12-07 17:15:15 +0800

    add initial dotfiles, bin scripts, and Linux setup

---
+++ Linux/dev-client-libs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt-get install postgresql-client-common \
+	postgresql-client-9.6 \
+	mongodb-clients \
+	mongodb-org-shell \
+	mongodb-org-tools \
+	redis-tools
---
+++ Linux/games.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y cowsay fortune sl cmatrix
