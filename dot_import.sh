1953feadecea43b1827ed6c7aec7ecace3d31d7e
Mhd Sami Al Mouhtaseb <sami.al-mouhtaseb@klarna.com>
2021-12-07 10:17:07 +0100

    add history_preexec.sh to dot_import locations

--- dot_import.sh
+++ dot_import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LOCATIONS="aliases bash_profile editorconfig exports functions gemrc gitconfig gitignore_global hushlogin psqlrc vimrc zshrc"
+LOCATIONS="aliases bash_profile editorconfig exports functions gemrc gitconfig gitignore_global hushlogin psqlrc vimrc zshrc history_preexec.sh"
 
 for location in $LOCATIONS; do
 	cp ~/dotfiles/.$location ~/.$location
