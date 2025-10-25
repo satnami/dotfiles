d9b8f203367e2a8b015da179ab3804b69b878e70
Mhd Sami Al Mouhtaseb <mhd.sami.al.mouhtaseb@wolt.com>
2025-10-26 00:08:18 +0200

    simplify ghostty setup.sh to a single copy command

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Create a directory for the Ghostty config file
-mkdir -p ~/.config/ghostty
-
-# Create a symlink to the actual config file
-ln -sf "$HOME/.config/ghostty/config" "$HOME/dotfiles/ghostty/config"
-
-ln -sf "$HOME/Library/Application Support/com.mitchellh.ghostty/config" "$HOME/dotfiles/ghostty/config"
- 
-# Check the symlink
-ls -l $HOME/dotfiles/ghostty/config
+cp config "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
